7f06cf914cbefcac1a55654f1b5c5a4cf087b490
parangbee <parangbee@gmail.com>
2019-05-11 15:08:15 +0900

    test

--- tradeTest/chaincode/tradeWorkflow/tradeWorkflow.go
+++ tradeTest/chaincode/tradeWorkflow/tradeWorkflow.go
@@ -7,17 +7,34 @@ import (
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
+// TradeWorkflowChaincode implementation
 type TradeWorkflowChaincode struct {
 	testMode bool
 }
 
+// Init implementation
 func (t *TradeWorkflowChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("Init Tradeworkflow")
+	fmt.Println("Init TradeWorkflowChaincode")
+	fname, args := stub.GetFunctionAndParameters)
+	var err error
+
+	if len(args) == 0 {
+		return shim.Success(nil)
+	}
+
+	if len(args) != 8 {
+		err = errors.New(fmt.Sprintf("Incorrect number of arguments, Expecting 8:" +
+			"Exporter, Exporter's Bank, Exporter Account Balance, Importer, Importer's Bank, " +
+			"Importer Account Balance, Carrier, Requlatory Authority"))
+		return shim.Error(err.Error())
+	}
+
 	return shim.Success(nil)
 }
 
+// Invoke implementation
 func (t *TradeWorkflowChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("Invoke TradeWorkflow")
+	fmt.Println("Invoke TradeWorkflowChaincode")
 	return shim.Success(nil)
 }
 
