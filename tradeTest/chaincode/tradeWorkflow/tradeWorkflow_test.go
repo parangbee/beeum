9b2c9ac9f0b8e192048896a7e921819d52eac5a3
parangbee <parangbee@gmail.com>
2019-05-16 22:03:30 +0900

    test

---
+++ tradeTest/chaincode/tradeWorkflow/tradeWorkflow_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+func TestTradeWrokflowInit(t *testing.T) {
+	twf := new(TradeWorkflowChaincode)
+	twf.testMode = true
+	mstub := shim.NewMockStub("TradeWorkflow", twf)
+
+	args := [][]byte{[]byte("Init"),
+		[]byte("Importer Inc"),
+		[]byte("Importer Bank"),
+		[]byte("1000000"),
+		[]byte("Exporter Inc"),
+		[]byte("Exporter Bank"),
+		[]byte("1000000"),
+		[]byte("KAL"),
+		[]byte("Authority Department")}
+
+	//check Init
+	res := mstub.MockInit("1", args)
+
+	if res.Status != shim.OK {
+		fmt.Println("[Test] Init Failed", string(res.Message))
+		t.FailNow()
+	}
+}
